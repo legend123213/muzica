436f14c2e6217953d375552f905b5bdfa9a90542
legend <abelwendmu124@gmail.com>
2023-04-05 14:54:28 +0300

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // this will allow you to import images like `import image from '@/assets/image.png'`
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  // configure how assets are processed and emitted
-  assetsInclude: /\.(png|jpe?g|gif|svg)$/i,
 });
