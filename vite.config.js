547558b4e81307b94f1838e4739a6102b47d05cc
legend <abelwendmu124@gmail.com>
2023-04-05 14:48:14 +0300

    public comm

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // this will allow you to import images like `import image from '@/assets/image.png'`
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // configure how assets are processed and emitted
+  assetsInclude: /\.(png|jpe?g|gif|svg)$/i,
+});
